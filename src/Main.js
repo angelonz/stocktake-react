3875d2e67652f124f748635986cdadeede672ece
Angelo <angeloangeles.nz@gmail.com>
2017-06-01 16:11:05 +1200

    moved dispatch call to lifecycle method

--- src/Main.js
+++ src/Main.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import Landing from './Landing';
 import Register from './Register';
@@ -11,35 +11,42 @@ import { withRouter } from 'react-router-dom';
 import ProtectedRoute from './ProtectedRoute';
 import authUtil from './utils/authUtil';
 
-const Main = (props) => {
+class Main extends Component {
 
-    if (!authUtil.isAuthenticated()) {
-        props.dispatch({
-            type: 'NOT_AUTHENTICATED'
-        });
+    componentDidMount() {
+        if (!authUtil.isAuthenticated()) {
+            this.props.dispatch({
+                type: 'NOT_AUTHENTICATED'
+            });
+        }
     }
 
-    return (
-        <Switch>
-            <Route exact path='/' component={Landing}/>
-            <ProtectedRoute path='/dashboard' component={Dashboard} />
-            <Route path='/register' render={() => {
-                return (
-                    props.authenticated ? <Redirect to='/dashboard' /> : <Register />
-                )
-            }} />
-            <Route path='/activate' component={AccountActivation}/>
-            <Route path='/verify' component={Login} />
-            <Route path='/login' component={Login} />
-            <Route path='/logout' render={() => {
-                props.dispatch({
-                    type: 'LOGOUT'
-                });
-                return <Redirect to='/'/>
-            }} />
-            <Route component={NoMatch}/>
-        </Switch>
-    );
+    render() {
+
+        return (
+            <Switch>
+                <Route exact path='/' component={Landing}/>
+                <ProtectedRoute path='/dashboard' component={Dashboard} />
+                <Route path='/register' render={() => {
+                    return (
+                        this.props.authenticated ? <Redirect to='/dashboard' /> : <Register />
+                    )
+                }} />
+                <Route path='/activate' component={AccountActivation}/>
+                <Route path='/verify' component={Login} />
+                <Route path='/login' component={Login} />
+                <Route path='/logout' render={() => {
+                    this.props.dispatch({
+                        type: 'LOGOUT'
+                    });
+                    return <Redirect to='/'/>
+                }} />
+                <Route component={NoMatch}/>
+            </Switch>
+        );
+
+    }
+    
 };
 
 const mapStateToProps = (state) => {
